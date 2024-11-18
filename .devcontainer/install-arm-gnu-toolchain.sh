874fa0783e1134a32cbd66de21df5e7ff1db848a
Matthias Sokolowski <matthias.sokolowski@gmail.com>
2024-11-18 20:22:40 +0100

    fix: temporarily downpatch gnu-arm-toolchain to 13.2

--- .devcontainer/install-arm-gnu-toolchain.sh
+++ .devcontainer/install-arm-gnu-toolchain.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 set -e -x
 
+# There current 13.3 version was removed from the download server (?).
+# In the meantime the 13.2 is used...
+
+# cd /opt/
+# wget https://developer.arm.com/-/media/Files/downloads/gnu/13.3.rel1/binrel/arm-gnu-toolchain-13.3.rel1-x86_64-arm-none-eabi.tar.xz
+# xz -d arm-gnu-toolchain-13.3.rel1-x86_64-arm-none-eabi.tar.xz
+# tar -xvf arm-gnu-toolchain-13.3.rel1-x86_64-arm-none-eabi.tar
+# rm arm-gnu-toolchain-13.3.rel1-x86_64-arm-none-eabi.tar
+# mv arm-gnu-toolchain-13.3.rel1-x86_64-arm-none-eabi arm-gnu-toolchain
+
 cd /opt/
-wget https://developer.arm.com/-/media/Files/downloads/gnu/13.3.rel1/binrel/arm-gnu-toolchain-13.3.rel1-x86_64-arm-none-eabi.tar.xz
-xz -d arm-gnu-toolchain-13.3.rel1-x86_64-arm-none-eabi.tar.xz
-tar -xvf arm-gnu-toolchain-13.3.rel1-x86_64-arm-none-eabi.tar
-rm arm-gnu-toolchain-13.3.rel1-x86_64-arm-none-eabi.tar
-mv arm-gnu-toolchain-13.3.rel1-x86_64-arm-none-eabi arm-gnu-toolchain
+wget https://developer.arm.com/-/media/Files/downloads/gnu/13.2.rel1/binrel/arm-gnu-toolchain-13.2.rel1-x86_64-arm-none-eabi.tar.xz
+xz -d arm-gnu-toolchain-13.2.rel1-x86_64-arm-none-eabi.tar.xz
+tar -xvf arm-gnu-toolchain-13.2.rel1-x86_64-arm-none-eabi.tar
+rm arm-gnu-toolchain-13.2.rel1-x86_64-arm-none-eabi.tar
+mv arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi arm-gnu-toolchain
