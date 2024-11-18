e105a30b488bd684928ea27803659447686cc158
Matthias Sokolowski <matthias.sokolowski@gmail.com>
2024-11-18 01:07:56 +0100

    chore: pin versions of openocd and picotool

--- .devcontainer/install-openocd.sh
+++ .devcontainer/install-openocd.sh
@@ -6,7 +6,8 @@ apt install -y libtool pkg-config libhidapi-libusb0 libhidapi-dev libusb-dev lib
 cd /opt/
 git clone --recursive https://github.com/raspberrypi/openocd
 cd openocd
-
+# hash suitable for the 2.0.0 pico-SDK
+git checkout ebec9504d7ad2fbd7a64d60dace013267d80172d
 ./bootstrap
 ./configure --enable-cmsis-dap
 make -j
--- .devcontainer/install-picotool.sh
+++ .devcontainer/install-picotool.sh
@@ -4,6 +4,7 @@ set -e -x
 cd /opt/
 git clone https://github.com/raspberrypi/picotool
 cd picotool/
+git checkout 2.0.0
 mkdir build
 cd build/
 cmake -D PICO_SDK_PATH=/opt/pico-sdk/ ..
