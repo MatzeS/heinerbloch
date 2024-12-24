5c95fe95b499f5d9a99dc60f4a7d92e4608f4d84
Matthias Sokolowski <matthias.sokolowski@gmail.com>
2024-12-24 19:24:29 +0000

    feat: add byteswap

--- include/heinerbloch/Utilities.hpp
+++ include/heinerbloch/Utilities.hpp
@@ -1,6 +1,22 @@
 #pragma once
 
+#include <algorithm>
+#include <array>
+#include <bit>
 #include <cstddef>
+#include <ranges>
+#include <type_traits>
 
 /// Tests that all bits set in 'test' are set in 'input'.
 bool testBits(std::byte input, std::byte test);
+
+// Copied from https://en.cppreference.com/w/cpp/numeric/byteswap
+template <std::integral T>
+constexpr T byteswap(T value) noexcept {
+  static_assert(std::has_unique_object_representations_v<T>,
+                "T may not have padding bits");
+  auto value_representation =
+      std::bit_cast<std::array<std::byte, sizeof(T)>>(value);
+  std::ranges::reverse(value_representation);
+  return std::bit_cast<T>(value_representation);
+}
--- test/UtilitiesTest.cpp
+++ test/UtilitiesTest.cpp
@@ -11,3 +11,8 @@ TEST(Utilities, test_bits_rejects_unset_bits) {
   EXPECT_FALSE(testBits(std::byte{0x0F}, std::byte{0b01000000}));
   EXPECT_FALSE(testBits(std::byte{0x0F}, std::byte{0b01000001}));
 }
+
+TEST(Utilities, byteswap) {
+  EXPECT_EQ(byteswap(uint8_t{0x12}), uint8_t{0x12});
+  EXPECT_EQ(byteswap(uint16_t{0xABCD}), uint16_t{0xCDAB});
+}
