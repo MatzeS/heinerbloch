3f848978b1e1208f915b2a6cba1d6cf818953d6c
Matthias Sokolowski <matthias.sokolowski@gmail.com>
2024-12-25 22:46:40 +0000

    chore: apply new formatting rules

--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -1,6 +1,6 @@
 #include "heinerbloch/Utilities.hpp"
 
 bool testBits(std::byte input, std::byte test) {
-  auto const masked = input & test;
-  return masked == test;
+    auto const masked = input & test;
+    return masked == test;
 }
